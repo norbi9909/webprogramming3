5ddbd6708102fb7ea01654979fbc84effcb9d8c0
Norbi <norbi9909@gmail.com>
2020-03-13 10:21:13 +0200

    Add index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+	<div class="jumbotron bg-primary">
+	  <h1 class="display-4">Socialgram</h1>
+	  <p class="lead">We'd like to provie a greate way to communicate with your friends during these hard corona filled days...</p>
+	  <hr class="my-4">
+	  <p>Feel free to share somehting interesting so your friends can see how cool you are ;)</p>
+	  <a class="btn btn-dark btn-lg" href="/about" role="button">Learn more</a>
+	</div>
+@endsection
