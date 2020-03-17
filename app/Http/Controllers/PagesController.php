9a5a1dbfe7fac5bf067c3d8024728d0740eac6eb
Norbi <norbi9909@gmail.com>
2020-03-17 10:10:17 +0200

    Add PagesConrtroller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function index() {
+    	return view("index");
+    }
+
+    function about() {
+    	return view("about");
+    }
+}
